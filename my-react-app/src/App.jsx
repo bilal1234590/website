0ce28bc42ab767a0231c0466f9e1af637840031e
bilalg <girachbilal8@gmail.com>
2024-09-14 20:59:39 +0530

    intro

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,13 @@
-import Header from "./header"
+import Header from "./header";
+import Footer from "./Footer";
+import Food from "./Food";
 function App() {
   return(
-    <Header></Header>
+    <>
+    <Header/>
+    <Food/>
+    <Footer/>
+    </>
   );
 }
 
