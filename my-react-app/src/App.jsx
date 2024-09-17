38a1def868da1a0dcff317ef2c7284defac7a152
bilalg <girachbilal8@gmail.com>
2024-09-17 20:04:36 +0530

    chapter 3 end

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 
 function App() {
   return (
-  <Button/>);
+  <Button />);
 }
 
 export default App
--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -1,9 +1,17 @@
-
-
 function Button(){
-    return(
-        <button className="button">Click me!</button>
-    )
+
+const styles = {
+        backgroundcolor: "hsl(200, 100%, 50%)",
+        color: "white",
+        padding: "10px 20px",
+        borderradius: "5px",
+        border: "none",
+        cursor: "pointer",
+      }
+
+
+      
+return(<button style={styles} >Click me!</button>)
 }
 
 export default  Button;
